b1a20a37c8134b97b52d2d2b9b921a0bb7a73a55
kjw1237 <kjw1237@gmail.com>
2021-07-01 18:17:39 +0900

    VO

--- src/main/java/com/blog/mvc/bbs/BbsVO.java
+++ src/main/java/com/blog/mvc/bbs/BbsVO.java
@@ -11,4 +11,32 @@ import lombok.Data;
  **/
 @Data
 public class BbsVO {
+    /**
+     * SYS_BBS
+     *
+     * board_num        varchar(11)         NOTNULL
+     * board_title      varchar(100)        NOTNULL
+     * board_content    varchar(5000)       NOTNULL
+     * category1        varchar(45)         NOTNULL
+     * category2        varchar(45)         NOTNULL
+     * tag              varchar(300)
+     * regdate          varchar(45)         NOTNULL
+     * moddate          varchar(45)
+     * img              varchar(100)
+     * uid              varchar(45)         NOTNULL
+     * hit              int(11)
+     * **/
+
+    String board_num;
+    String board_title;
+    String board_content;
+    String category1;
+    String category2;
+    String tag;
+    String regdate;
+    String moddate;
+    String img;
+    String uid;
+    int hit;
+    String test;
 }
--- src/main/java/com/blog/mvc/user/UserVO.java
+++ src/main/java/com/blog/mvc/user/UserVO.java
@@ -12,4 +12,27 @@ import lombok.Data;
  **/
 @Data
 public class UserVO {
+    /**
+     * USER
+     *
+     * uid          varchar(45)         NOTNULL
+     * upwd         varchar(100)        NOTNULL
+     * uname        varchar(30)         NOTNULL
+     * uemail       varchar(45)         NOTNULL
+     * email_flag   varchar(45)         NOTNULL
+     * pwd_flag     varchar(45)         NOTNULL
+     * flag         varchar(45)         NOTNULL
+     * regdate      varchar(45)         NOTNULL
+     * moddate      varchar(45)
+     * **/
+    int result;
+    String uid;
+    String upwd;
+    String uname;
+    String uemail;
+    String email_flag;
+    String pwd_flag;
+    String flag;
+    String regdate;
+    String moddate;
 }
