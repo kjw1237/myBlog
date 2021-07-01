920cd98a38cb95c7ad76b902e48c2ebe631bf061
JerryPigs <w8230@naver.com>
2021-07-01 18:14:40 +0900

    base settings

--- src/main/java/com/blog/mvc/user/UserController.java
+++ src/main/java/com/blog/mvc/user/UserController.java
@@ -1,6 +1,12 @@
 package com.blog.mvc.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @package : com.blog.mvc.user
@@ -12,4 +18,18 @@ import org.springframework.stereotype.Controller;
  **/
 @Controller
 public class UserController {
+    //로그인 페이지 이동
+    @RequestMapping(value ="/user/login")
+    public ModelAndView openLoginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("body/user/login");
+        return mav;
+    }
+    //가입 페이지 이동
+    @RequestMapping(value = "user/join")
+    public ModelAndView openJoinPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("body/user/join");
+        return mav;
+    }
 }
